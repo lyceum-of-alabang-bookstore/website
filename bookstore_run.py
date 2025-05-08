99c86870a90bc65624fd009778fe5e2d5c8feb0e
lyceum-of-alabang-bookstore <lyceumofalabangbookstore@gmail.com>
2025-05-09 00:17:10 +0800

    Add basic auth to /admin route

--- bookstore_run.py
+++ bookstore_run.py
@@ -74,6 +74,7 @@ def order_summary():
     return render_template('order_summary.html')
 
 @app.route('/admin')
+@requires_auth
 def admin():
     with sqlite3.connect(DB_FILE) as conn:
         conn.row_factory = sqlite3.Row
